430e1050dcd118935430720ada37309170b5159d
AkifSivas58 <mehmetakifsivas58@gmail.com>
2026-02-06 19:01:25 +0300

    Initial commit: Project skeleton generated via Spring Initializr

---
+++ src/main/java/com/cvproject/netsentience/NetsentienceApplication.java
@@ -0,0 +1,13 @@
+package com.cvproject.netsentience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetsentienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetsentienceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvproject/netsentience/NetsentienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvproject.netsentience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetsentienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
